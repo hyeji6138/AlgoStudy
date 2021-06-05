9d65ed369b598fe1989dd6b4f80ebb335471c3af
hyeji6138 <hyeji6138@naver.com>
2021-06-05 22:53:08 +0900

    baekjoon 1005 ACM Craft

--- Baekjoon/src/gold3/Solution_1005.java
+++ Baekjoon/src/gold3/Solution_1005.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution_1005 {
+public class Solution_1005 { // ACM Craft
 	static int T, N, K, W;
 	static int[] delay;
 	static boolean[][] graph;
