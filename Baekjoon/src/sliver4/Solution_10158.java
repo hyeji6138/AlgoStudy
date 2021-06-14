81700df4e28bb6c07a63e38d9c4511b396e4f565
hyeji6138 <hyeji6138@naver.com>
2021-06-14 21:17:00 +0900

    baekjoon 10158 개미

---
+++ Baekjoon/src/sliver4/Solution_10158.java
@@ -0,0 +1,30 @@
+package sliver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_10158 {
+	static int w, h, p, q;
+	static int t;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		p = Integer.parseInt(st.nextToken());
+		q = Integer.parseInt(st.nextToken());
+		t = Integer.parseInt(br.readLine());
+		// ==============입력 끝===================
+		int pt = (p + t) % (w * 2);
+		int qt = (q + t) % (h * 2);
+
+		if (pt > w) pt = w * 2 - pt;
+		if (qt > h) qt = h * 2 - qt;
+
+		System.out.println(pt + " " + qt);
+	}
+}
