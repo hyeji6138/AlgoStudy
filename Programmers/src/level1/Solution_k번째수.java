0b8d30056dca68e57710cf64be3cab25c11b25fe
hyeji6138 <hyeji6138@naver.com>
2021-06-30 22:30:37 +0900

    programmers level1 k번째수

---
+++ Programmers/src/level1/Solution_k번째수.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.Arrays;
+
+public class Solution_k번째수 {
+	public static void main(String[] args) {
+		int[] array= {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		System.out.println(Arrays.toString(solution(array, commands)));
+	}
+	public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i=0;i<commands.length;i++) {
+        	int[] cutArray = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+        	Arrays.sort(cutArray);
+        	answer[i] = cutArray[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+
+}
