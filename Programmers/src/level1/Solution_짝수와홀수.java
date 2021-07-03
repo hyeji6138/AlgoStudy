49f4b6cdc5b3cca583458e199bb1f87765e1c8d4
hyeji6138 <hyeji6138@naver.com>
2021-07-03 18:37:43 +0900

    programmers level1 짝수와 홀수

---
+++ Programmers/src/level1/Solution_짝수와홀수.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class Solution_짝수와홀수 {
+	public static void main(String[] args) {
+		System.out.println(solution(3));
+	}
+	public static String solution(int num) {
+        String answer = "";
+        if(num%2==0) answer = "Even";
+        else answer = "Odd";
+        return answer;
+    }
+}
