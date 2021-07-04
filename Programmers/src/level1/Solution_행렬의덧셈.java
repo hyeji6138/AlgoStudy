926e983614a713dd8e8101fa51d054263499d950
hyeji6138 <hyeji6138@naver.com>
2021-07-04 13:21:09 +0900

    programmers level1 행렬의 덧셈

---
+++ Programmers/src/level1/Solution_행렬의덧셈.java
@@ -0,0 +1,27 @@
+package level1;
+
+public class Solution_행렬의덧셈 {
+	public static void main(String[] args) {
+		int[][] arr1 = {}, arr2 = {};
+		printArray(solution(arr1, arr2));
+	}
+
+	public static int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr1[0].length];
+		for (int i = 0; i < arr1.length; i++) {
+			for (int j = 0; j < arr1[i].length; j++) {
+				answer[i][j] = arr1[i][j] + arr2[i][j];
+			}
+		}
+		return answer;
+	}
+
+	public static void printArray(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
