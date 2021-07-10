3e58a8fb88be9ca5f1e3b67aa560f53c56130647
hyeji6138 <hyeji6138@naver.com>
2021-07-10 21:51:03 +0900

    programmers level2 괄호 변환

--- Programmers/src/level2/Solution_괄호변환.java
+++ Programmers/src/level2/Solution_괄호변환.java
@@ -28,22 +28,20 @@ public class Solution_괄호변환 {
 					break;
 				}
 			}
-			System.out.println(check+" "+p.substring(0, std + 1));
 			if (check) {
-				answer = p.substring(0, std + 1);
+				answer = p.substring(0, std + 1) + solution(p.substring(std + 1));
 			} else {
 				StringBuffer sb = new StringBuffer();
 				String temp = p.substring(1, std);
 				sb.append('(');
+				sb.append(solution(p.substring(std + 1)));
+				sb.append(')');
 				for (int i = 0; i < temp.length(); i++) {
 					if (temp.charAt(i) == '(') sb.append(')');
 					else sb.append('(');
 				}
-				sb.append(')');
 				answer = sb.toString();
 			}
-
-			answer = answer + solution(p.substring(std + 1));
 		}
 		return answer;
 	}
