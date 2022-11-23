6456e3a5d668d997529bf87d84e999411fce42d4
hyeji6138 <hyeji6138@naver.com>
2022-11-23 19:39:44 +0900

    221123 | 프로그래머스 예상 대진표

--- Programmers/src/level2/Solution_예상대진표.java
+++ Programmers/src/level2/Solution_예상대진표.java
@@ -2,23 +2,28 @@ package level2;
 
 public class Solution_예상대진표 {
 	public static void main(String[] args) {
-		System.out.println(solution(16, 12, 16));
+		System.out.println(solution(1024, 1, 899));
 	}
 
 	public static int solution(int n, int a, int b) {
 		int answer = 0;
-		answer = test(n, a, b);
+		while (true) {
+			answer++;
+			System.out.println(a + " " + b);
+			if (Math.abs(a - b) == 1 && Math.max(a, b) / 2 - 1 == Math.min(a, b) / 2) {
+				break;
+			}
+			a = nextNumber(a);
+			b = nextNumber(b);
+		}
 		return answer;
 	}
 
-	public static int test(int n, int a, int b) {
-		if(n == 1) {
-			return 1;
+	public static int nextNumber(int n) {
+		if (n % 2 == 0) {
+			return n / 2;
+		} else {
+			return n / 2 + 1;
 		}
-		int cnt = 0;
-		if ((a <= n / 2 && b <= n / 2) || (a > n / 2 && b > n / 2)) {
-			cnt += test(n / 2, a / 2 + a % 2, b / 2 + b % 2);
-		}
-		return cnt;
 	}
 }
