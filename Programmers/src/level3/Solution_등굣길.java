8cf0ea07dfc45c0553acc8d911dfca84d827339b
hyeji6138 <hyeji6138@naver.com>
2022-12-30 18:45:42 +0900

    221230 | 프로그래머스 등굣길(수정)

--- Programmers/src/level3/Solution_등굣길.java
+++ Programmers/src/level3/Solution_등굣길.java
@@ -32,39 +32,8 @@ public class Solution_등굣길 {
 					}
 					map[i][j] = map[i][j] + (top + left) % 1000000007;
 				}
-				System.out.println(i + " " + j + " " + map[i][j]);
 			}
 		}
-
-//		int answer = 0;
-//		int[][] map = new int[m][n];
-//		boolean[][] puddle = new boolean[m][n];
-//		boolean[][] visit = new boolean[m][n];
-//		for (int i = 0; i < puddles.length; i++) {
-//			puddle[puddles[i][0] - 1][puddles[i][1] - 1] = true;
-//		}
-//		Queue<int[]> queue = new LinkedList<>();
-//		queue.add(new int[] { 0, 0 });
-//		visit[0][0] = true;
-//		map[0][0] = 1;
-//		while (!queue.isEmpty()) {
-//			int[] point = queue.poll();
-//
-//			for (int d = 0; d < di.length; d++) {
-//				int ni = point[0] + di[d];
-//				int nj = point[1] + dj[d];
-//
-//				if (ni >= 0 && ni < m && nj >= 0 && nj < n && !puddle[ni][nj]) {
-//					if (!visit[ni][nj]) {
-//						visit[ni][nj] = true;
-//						queue.add(new int[] { ni, nj });
-//					}
-//					map[ni][nj] = (map[ni][nj] + map[point[0]][point[1]]) % 1000000007;
-//				}
-//			}
-//
-//		}
-//		answer = map[m - 1][n - 1];
 		answer = map[n - 1][m - 1];
 		return answer;
 	}
