da1b44228db645287c8ff1b2e04882cb2e56a332
hyeji6138 <hyeji6138@naver.com>
2023-01-12 20:09:18 +0900

    230112 | 프로그래머스 징검다리 건너기 (효율성 수정중)

--- Programmers/src/level3/Solution_징검다리건너기.java
+++ Programmers/src/level3/Solution_징검다리건너기.java
@@ -1,29 +1,58 @@
 package level3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Solution_징검다리건너기 {
 	public static void main(String[] args) {
 		int[] stones = { 2, 4, 5, 3, 2, 1, 4, 2, 5, 1 };
 		int k = 3;
 		System.out.println(solution(stones, k));
 	}
-
+// 효율성 13/14
 	public static int solution(int[] stones, int k) {
 		int answer = Integer.MAX_VALUE;
-		List<Integer> list = new ArrayList<>();
+		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < k; i++) {
-			list.add(stones[i]);
+			if (max < stones[i]) {
+				max = stones[i];
+			}
 		}
-		answer = Math.min(answer, Collections.max(list));
+		answer = Math.min(answer, max);
 		for (int i = k; i < stones.length; i++) {
-			list.remove(0);
-			list.add(stones[i]);
-			answer = Math.min(answer, Collections.max(list));
+			if (max < stones[i]) {
+				max = stones[i];
+			} else {
+				if (max == stones[i - k]) {
+					max = findMax(i, k, stones);
+				}
+			}
+			answer = Math.min(answer, max);
 		}
 		return answer;
 	}
+
+	public static int findMax(int idx, int k, int[] stones) {
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < k; i++) {
+			if (max < stones[idx - i]) {
+				max = stones[idx - i];
+			}
+		}
+		return max;
+	}
+
+// 효율성 0/14
+//	public static int solution(int[] stones, int k) {
+//		int answer = Integer.MAX_VALUE;
+//		List<Integer> list = new ArrayList<>();
+//		for (int i = 0; i < k; i++) {
+//			list.add(stones[i]);
+//		}
+//		answer = Math.min(answer, Collections.max(list));
+//		for (int i = k; i < stones.length; i++) {
+//			list.remove(0);
+//			list.add(stones[i]);
+//			answer = Math.min(answer, Collections.max(list));
+//		}
+//		return answer;
+//	}
 
 }
