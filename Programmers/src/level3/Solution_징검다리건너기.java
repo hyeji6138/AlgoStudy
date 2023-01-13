5e88ce3db59b17b8415e14b916180f01c7ed05cf
hyeji6138 <hyeji6138@naver.com>
2023-01-13 11:47:48 +0900

    230113 | 프로그래머스 징검다리 건너기(수정)

--- Programmers/src/level3/Solution_징검다리건너기.java
+++ Programmers/src/level3/Solution_징검다리건너기.java
@@ -6,38 +6,65 @@ public class Solution_징검다리건너기 {
 		int k = 3;
 		System.out.println(solution(stones, k));
 	}
-// 효율성 13/14
+
+// 이진탐색
 	public static int solution(int[] stones, int k) {
-		int answer = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < k; i++) {
-			if (max < stones[i]) {
-				max = stones[i];
-			}
-		}
-		answer = Math.min(answer, max);
-		for (int i = k; i < stones.length; i++) {
-			if (max < stones[i]) {
-				max = stones[i];
-			} else {
-				if (max == stones[i - k]) {
-					max = findMax(i, k, stones);
+		int left = 1, right = 200000000;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			int cnt = 0;
+			for (int i = 0; i < stones.length; i++) {
+				if (stones[i] - mid <= 0) {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+
+				if (cnt == k) {
+					break;
 				}
 			}
-			answer = Math.min(answer, max);
-		}
-		return answer;
-	}
-
-	public static int findMax(int idx, int k, int[] stones) {
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < k; i++) {
-			if (max < stones[idx - i]) {
-				max = stones[idx - i];
+			if (cnt == k) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
 			}
 		}
-		return max;
+		return left;
 	}
+
+// 효율성 13/14
+//	public static int solution(int[] stones, int k) {
+//		int answer = Integer.MAX_VALUE;
+//		int maxIdx = 0;
+//		for (int i = 0; i < k; i++) {
+//			if (stones[maxIdx] <= stones[i]) {
+//				maxIdx = i;
+//			}
+//		}
+//		answer = Math.min(answer, stones[maxIdx]);
+//		for (int i = k; i < stones.length; i++) {
+//			if (stones[maxIdx] <= stones[i]) {
+//				maxIdx = i;
+//			} else {
+//				if (maxIdx == i - k) {
+//					maxIdx = findMax(i, k, stones);
+//				}
+//			}
+//			answer = Math.min(answer, stones[maxIdx]);
+//		}
+//		return answer;
+//	}
+//
+//	public static int findMax(int idx, int k, int[] stones) {
+//		int maxIdx = idx;
+//		for (int i = 0; i < k; i++) {
+//			if (stones[maxIdx] < stones[idx - i]) {
+//				maxIdx = idx - i;
+//			}
+//		}
+//		return maxIdx;
+//	}
 
 // 효율성 0/14
 //	public static int solution(int[] stones, int k) {
