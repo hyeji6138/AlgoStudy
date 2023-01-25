5252aacd4a179b584adab7c04c29df55f8675795
hyeji6138 <hyeji6138@naver.com>
2023-01-25 20:04:43 +0900

    230125 | 프로그래머스 가장 큰 정사각형 찾기

--- Programmers/src/level2/Solution_가장큰정사각형찾기.java
+++ Programmers/src/level2/Solution_가장큰정사각형찾기.java
@@ -1,57 +1,48 @@
 package level2;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Solution_가장큰정사각형찾기 {
-	static int[] di = { 0, 1, 1 }, dj = { 1, 1, 0 };
+	static int[] di = { -1, -1, 0 }, dj = { 0, -1, -1 };
 
 	public static void main(String[] args) {
-		int[][] board = { { 0, 0 }, { 0, 0 } };
+		int[][] board = { { 1 } };
 		System.out.println(solution(board));
 	}
 
 	public static int solution(int[][] board) {
 		int answer = 0;
 		int n = board.length, m = board[0].length;
-		boolean[][] visit = new boolean[n][m];
+		int[][] size = new int[n][m];
+		int max = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
-				if (board[i][j] > 0 && !visit[i][j]) {
-					Queue<int[]> queue = new LinkedList<int[]>();
-					queue.add(new int[] { i, j });
-					visit[i][j] = true;
-					int time = 1;
-					boolean go = true;
-					while (!queue.isEmpty()) {
-						if (!go) {
+				if (board[i][j] == 1) {
+					int min = Integer.MAX_VALUE;
+					boolean sqr = true;
+					for (int d = 0; d < di.length; d++) {
+						int ni = i + di[d];
+						int nj = j + dj[d];
+						if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
+							if (min > size[ni][nj]) {
+								min = size[ni][nj];
+							}
+						} else {
+							sqr = false;
 							break;
 						}
-						answer = Math.max(answer, time * time);
-						int size = queue.size();
-						for (int s = 0; s < size && go; s++) {
-							int[] p = queue.poll();
-
-							for (int d = 0; d < di.length; d++) {
-								int ni = p[0] + di[d];
-								int nj = p[1] + dj[d];
-
-								if (ni >= 0 && ni < n && nj >= 0 && nj < m && board[ni][nj] > 0) {
-									if (!visit[ni][nj]) {
-										queue.add(new int[] { ni, nj });
-										visit[ni][nj] = true;
-									}
-								} else {
-									go = false;
-									break;
-								}
-							}
-						}
-						time++;
 					}
+					if (sqr) {
+						size[i][j] = min + 1;
+					} else {
+						size[i][j] = board[i][j];
+					}
+					max = Math.max(max, size[i][j]);
 				}
+				System.out.print(size[i][j] + " ");
 			}
+			System.out.println();
 		}
+
+		answer = max * max;
 		return answer;
 	}
 
