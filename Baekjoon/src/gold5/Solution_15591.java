33f6ad446738167e11bec8218a7aee02e99d33e2
hyeji6138 <hyeji6138@naver.com>
2023-05-31 21:11:48 +0900

    230531 | 백준 15591 MooTube

--- Baekjoon/src/gold5/Solution_15591.java
+++ Baekjoon/src/gold5/Solution_15591.java
@@ -3,7 +3,9 @@ package gold5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -17,45 +19,37 @@ public class Solution_15591 {
 		N = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
 
-		int[][] video = new int[N + 1][N + 1];
-
+		List<int[]>[] USADO = new ArrayList[N + 1]; // 연결 상태 저장할 때 배열 사용 시 시간초과
+		for (int i = 1; i <= N; i++) {
+			USADO[i] = new ArrayList<>();
+		}
 		for (int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			video[a][b] = v;
-			video[b][a] = v;
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			USADO[p].add(new int[] { q, r });
+			USADO[q].add(new int[] { p, r });
 		}
-
-//		for (int i = 1; i <= N; i++) {
-//			for (int j = 1; j <= N; j++) {
-//				System.out.print(link[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
 
 		for (int t = 0; t < Q; t++) {
 			st = new StringTokenizer(br.readLine());
 			int k = Integer.parseInt(st.nextToken());
 			int start = Integer.parseInt(st.nextToken());
 			int answer = 0;
-			Queue<int[]> queue = new LinkedList<>();
+
+			Queue<Integer> queue = new LinkedList<>();
 			boolean[] visit = new boolean[N + 1];
 			visit[start] = true;
-			queue.add(new int[] { start, Integer.MAX_VALUE });
+			queue.add(start);
 
 			while (!queue.isEmpty()) {
-				int[] p = queue.poll();
-				if (p[1] >= k) {
-					if (Integer.MAX_VALUE != p[1]) {
-						answer++;
-					}
-					for (int i = 1; i <= N; i++) {
-						if (!visit[i] && video[p[0]][i] != 0) {
-							visit[i] = true;
-							queue.add(new int[] { i, Math.min(p[1], video[p[0]][i]) });
-						}
+				int v = queue.poll();
+				for (int[] line : USADO[v]) {
+					if (!visit[line[0]] && k <= line[1]) {
+						answer += 1;
+						visit[line[0]] = true;
+						queue.add(line[0]);
 					}
 				}
 			}
