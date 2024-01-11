145a48555359d3c69e9850c698f643c168bf0e07
hyeji6138 <hyeji6138@naver.com>
2024-01-11 22:18:13 +0900

    240111 | 프로그래머스 level3 광고 삽입(수정중)

--- Programmers/src/level3/Solution_광고삽입.java
+++ Programmers/src/level3/Solution_광고삽입.java
@@ -1,90 +1,99 @@
 package level3;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Solution_광고삽입 {
 	public static void main(String[] args) {
-		String play_time = "99:59:59";
-		String adv_time = "25:00:00";
-		String[] logs = { "69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"};
+		String play_time = "02:03:55";
+		String adv_time = "00:14:15";
+		String[] logs = { "01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29",
+				"01:37:44-02:02:30" };
 
 		System.out.println(solution(play_time, adv_time, logs));
 
 	}
+
 	public static String solution(String play_time, String adv_time, String[] logs) {
 		String answer = "";
-		int[] play = strTimeToIntTime(play_time);
-		int[] adv = strTimeToIntTime(adv_time);
-		
-		int[][][] time = new int[play[0]+1][60][60];
-		for(int i=0;i<logs.length;i++) {
-			String[] watch_times = logs[i].split("-");
-			int[] start_time = strTimeToIntTime(watch_times[0]);
-			int[] end_time = strTimeToIntTime(watch_times[1]);
-			
-			int t=calTotalTime(start_time);
-			int end = calTotalTime(end_time);
-			while(t < end) {
-				int[] temp = sumTimeToIntTime(t);
-				time[temp[0]][temp[1]][temp[2]]++;
-				t++;
+		int playTime = strTimeToIntTime(play_time);
+		int advTime = strTimeToIntTime(adv_time);
+		int[][] log_time = new int[logs.length][2];
+		int[] maxPlayTime = new int[logs.length];
+		for (int i = 0; i < logs.length; i++) {
+			String[] log = logs[i].split("-");
+			log_time[i][0] = strTimeToIntTime(log[0]);
+			log_time[i][1] = strTimeToIntTime(log[1]);
+		}
+		Arrays.sort(log_time, new MyArraySort());
+		int point = -1;
+		for (int i = 0; i < log_time.length; i++) {
+			maxPlayTime[i] = Math.min(log_time[i][0] + advTime, log_time[i][1] - log_time[i][0]);
+			if (point < 0) {
+				point = log_time[i][1];
+			} else {
+				if (log_time[i][0] < point) { // 이전 시청자와 겹치는 구간 있음
+				} else {
+
+				}
 			}
+
 		}
-		int sumTime = 0;
-		int maxTime = 0;
-		int maxIdx = 0;
-		int playsum = calTotalTime(play);
-		int advsum = calTotalTime(adv);
-		int t=0;
-		while(t < advsum) {
-			int[] temp = sumTimeToIntTime(t);
-			sumTime += time[temp[0]][temp[1]][temp[2]];
-			t++;
-		}
-		
-		maxTime = sumTime;
-		
-		int prev = 0;
-		int next = advsum;
-		while(prev < playsum-advsum) {
-			int[] minusIdx = sumTimeToIntTime(prev);
-			int[] plusIdx =  sumTimeToIntTime(next);
-			sumTime = sumTime - time[minusIdx[0]][minusIdx[1]][minusIdx[2]] +time[plusIdx[0]][plusIdx[1]][plusIdx[2]]; 
-			if(maxTime < sumTime) {
-				maxTime = sumTime;
-				maxIdx = prev+1;
-			}
-			prev++;
-			next++;
-		}
-		int[] ans = sumTimeToIntTime(maxIdx);
-		StringBuffer sb = new StringBuffer();
-		for(int i=0;i<3;i++) {
-			if(ans[i]<10) sb.append("0");
-			sb.append(ans[i]+":");
-		}
-		answer = sb.substring(0, sb.length()-1);
 		return answer;
 	}
-	
-	public static int[] sumTimeToIntTime(int time) {
-		int[] result = {time/3600, (time/60)%60, time%60};
-		return result;
-	}
-	
-	public static int calTotalTime(int[] time) {
-		int result = 0;
-		for(int i=0;i<3;i++) {
-			result += (int)Math.pow(60, (2-i))*time[i];
-		}
-		return result;
-	}
-	
-	public static int[] strTimeToIntTime(String time) {
+
+	public static int strTimeToIntTime(String time) {
 		String[] times = time.split(":");
-		int[] result = new int[3];
-		for(int i=0;i<3;i++) {
-			result[i] = (times[i].charAt(0)-'0')*10 +(times[i].charAt(1)-'0'); 
-		}
+		int result = 0;
+		result += Integer.parseInt(times[0]) * 60 * 60;
+		result += Integer.parseInt(times[1]) * 60;
+		result += Integer.parseInt(times[2]);
 		return result;
+	}
+
+	public static String intTimeToStrTime(int time) {
+		StringBuffer st = new StringBuffer();
+		for (int i = 0; i < 2; i++) {
+			if (time % 60 < 10) {
+				st.insert(0, ":0" + time % 60);
+			} else {
+				st.insert(0, ":" + time % 60);
+			}
+			time /= 60;
+		}
+		if (time < 10) {
+			st.insert(0, "0" + time);
+		} else {
+			st.insert(0, "" + time);
+		}
+		return st.toString();
+	}
+
+	static class MyArraySort implements Comparator<int[]> {
+
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			// TODO Auto-generated method stub
+			if (o1[0] == o2[0]) {
+				return o1[1] - o2[1];
+			}
+			return o1[0] - o2[0];
+		}
+
+	}
+
+	static class MyQueueSort implements Comparator<int[]> {
+
+		@Override
+		public int compare(int[] o1, int[] o2) {
+			// TODO Auto-generated method stub
+			if (o1[0] == o2[0]) {
+				return o1[1] - o2[1];
+			}
+			return o2[0] - o1[0];
+		}
+
 	}
 
 }
